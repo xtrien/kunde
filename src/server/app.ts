a906f6a4b67f27522d2521d96e846e2e701f550e
Sofia <trso1011@hs-karlsruhe.de>
2019-05-13 16:36:33 +0200

    REST Implementierung
    Versuchte REST Implementierung

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,8 +1,10 @@
 // https://github.com/apollographql/apollo-server/tree/master/packages/apollo-server-express
 import { ApolloServer } from 'apollo-server-express'
+import * as bodyParser from 'body-parser'
 import * as express from 'express'
 
 import { verifyKunde } from './auth/jwt'
+import { login, alleKunden } from './db/mongo'
 import { resolvers } from './graphql/resolvers'
 import { typeDefs } from './graphql/typeDefs'
 import { logger } from './shared/logger'
@@ -26,6 +28,41 @@ app.get('/', (req, res) => {
 })
 
 // Todo: Rest-Schnittstelle
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+const basePath = '/rest'
+app.post(`${basePath}/login`, (request, response) => {
+    login(request.body).then((result) => {
+        response.send(result)
+    })
+})
+
+app.get(`${basePath}/kunden`, (request, response) => {
+    const token = request.headers.authorization
+    verifyKunde(token)
+        .then((kunde) => {
+            return alleKunden(kunde)
+        })
+        .then((alle) => {
+            response.send(alle)
+        })
+})
+
+app.get(`${basePath}/kunden/:id`, (request, response) => {
+    const token = request.headers.authorization
+
+})
+
+app.post(`${basePath}/kunden`,(request, response) => {
+    const receivedKunde = create('kunden', request.query)
+    if (receivedKunde) {
+        kunden.push(receivedKunde)
+        response.status(201).send(receivedKunde)
+    } else {
+        response.status(400).send()
+    }
+})
 
 app.listen({ port: 4000 }, () =>
     logger.info(
